4e1f5bb42fa297ce35a7e0019b4c6cb2e10f7400
tormozz48 <tormozz48@gmail.com>
2015-06-23 11:05:34 +0300

    Implement gracefull restart for *-builder services

--- bin/index.js
+++ bin/index.js
@@ -11,6 +11,11 @@ var fs = require('fs'),
 
         _bseConfig: undefined,
 
+        MAX_CALLS_FOR_NEW_EXECUTE_ITERATION: 20,
+        MAX_LAUNCH_COUNT: 5,
+        callsForNewExecuteIteration: 0,
+        launchCount: 0,
+
         loadConfig: function (callback) {
             var _this = this;
             this._logger.info('Load configuration file for bse-admin tool');
@@ -29,14 +34,54 @@ var fs = require('fs'),
         },
 
         execute: function () {
+
+            /**
+             * 1-я функция запланированного перезапуска сервиса.
+             * Происходит после завершения MAX_LAUNCH_COUNT сборки.
+             * Нужна для предотвращения случайного перезапуска сервиса в случае достижения лимита потребляемой памяти
+             */
+            function gracefulRestart1() {
+                if (this.launchCount === this.MAX_LAUNCH_COUNT) {
+                    logger.warn('Planned restart of service on reaching maximum launch count limit');
+                    process.exit(1);
+                } else {
+                    this.launchCount++;
+                }
+            }
+
+            /**
+             * 2-я функция запланированного перезапуска сервиса.
+             * Нужна для предотвращения "заклинивания" сборки. В случае когда сборка зависает
+             * и не заканчивается успешно или с ошибокой, попытки повторной сборки будут вызываться
+             * до тех пор пока их количество не превысит MAX_CALLS_FOR_NEW_EXECUTE_ITERATION
+             * после чего процесс будет перезапущен
+             */
+            function gracefulRestart2() {
+                if (this.callsForNewExecuteIteration === this.MAX_CALLS_FOR_NEW_EXECUTE_ITERATION) {
+                    logger.warn('Planned restart of service on reaching maximum calls for new iteration limit');
+                    process.exit(1);
+                } else {
+                    this.callsForNewExecuteIteration++;
+                }
+            }
+
             if (this['isActive']()) {
                 this._logger.warn('Another execute process is being performed now');
+                gracefulRestart2();
                 return;
             }
             this['setActive']();
             return this.__base()
-                .then(function () { this['setIdle'](); }, this)
-                .fail(function () { this['setIdle'](); }, this);
+                .then(function () {
+                    this.callsForNewExecuteIteration = 0;
+                    this['setIdle']();
+                    gracefulRestart1();
+                }, this)
+                .fail(function () {
+                    this.callsForNewExecuteIteration = 0;
+                    this['setIdle']();
+                    gracefulRestart1();
+                }, this);
         }
     }),
     tripWireConfig = fsExtra.readJSONFileSync('./config/_tripwire.json'),
